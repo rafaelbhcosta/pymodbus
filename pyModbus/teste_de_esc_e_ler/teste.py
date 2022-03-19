0be5e47da5fc1729a0d8aa2e4576771d1ddb554a
Rafael Costa <93993679+rafaelbhcosta@users.noreply.github.com>
2022-03-18 23:27:16 -0300

    Update teste.py

--- pyModbus/teste_de_esc_e_ler/teste.py
+++ pyModbus/teste_de_esc_e_ler/teste.py
@@ -7,3 +7,13 @@ from collections import OrderedDict
 
 client = ModbusClient('127.0.0.1', port=502)
 client.connect()
+
+builder = BinaryPayloadBuilder()
+
+address = int(input('Escolha o endereço da tabela ModBus: ')) - 1
+valor = int(input('Escolha 1 para True ou 0 para FALSE: '))
+builder.add_16bit_int(bool(valor))
+payload = builder.build()
+#client.write_coils(address, payload)
+client.write_coils(address, payload, skip_encode=True, unit=1)
+client.write_registers(address, payload, skip_encode=True, unit=1)
