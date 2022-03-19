74bd7ba320ab68d21247470ee4489c08ecb502f4
Rafael Costa <93993679+rafaelbhcosta@users.noreply.github.com>
2022-03-18 22:18:13 -0300

    att

---
+++ pyModbus/teste_de_esc_e_ler/teste.py
@@ -0,0 +1,9 @@
+from pymodbus.constants import Endian
+from pymodbus.payload import BinaryPayloadDecoder
+from pymodbus.payload import BinaryPayloadBuilder
+from pymodbus.client.sync import ModbusTcpClient as ModbusClient
+from pymodbus.compat import iteritems
+from collections import OrderedDict
+
+client = ModbusClient('127.0.0.1', port=502)
+client.connect()
