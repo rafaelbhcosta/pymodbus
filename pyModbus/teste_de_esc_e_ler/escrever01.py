c35e5e0043fecdb11db238ddb2449fc8e2d267d0
Rafael Costa <93993679+rafaelbhcosta@users.noreply.github.com>
2022-03-24 11:14:15 -0300

    Update escrever01.py

--- pyModbus/teste_de_esc_e_ler/escrever01.py
+++ pyModbus/teste_de_esc_e_ler/escrever01.py
@@ -32,44 +32,46 @@ if client.connect():
     print('-' *15)
     func = input('1- INT \n2- FLOAT \n3- STR \n4- BOLEANO \n5- DOOBLE \nEscolha: ')
 
-    #Função para escrita de números inteiros
-    if func == '1':
-        print('=' * 70)
-        address = int(input('Escolha o endereço da tabela ModBus: ')) - 1
-        valor = int(input('O valor INT que deve ser escrito: '))
-        builder.add_16bit_int(valor)
-        payload = builder.to_registers()
-        payload = builder.build()
-        client.write_registers(address, payload, skip_encode=True, unit=1)
 
-    #Função para escrita de números float
-    elif func == '2':
-        print('=' * 70)
-        address = int(input('Escolha o endereço da tabela ModBus: ')) - 1
-        valor = float(input('O valor FLOAT que deve ser escrito: '))
-        builder.add_32bit_float(valor)
-        payload = builder.to_registers()
-        payload = builder.build()
-        client.write_registers(address, payload, skip_encode=True, unit=1)
 
-    #Função para escrita de string
-    elif func == '3':
-        print('=' * 70)
-        address = int(input('Escolha o endereço da tabela ModBus: ')) - 1
-        valor = input('O que deve ser escrito: ')
-        builder.add_string(valor)
-        payload = builder.to_registers()
-        payload = builder.build()
-        client.write_registers(address, payload, skip_encode=True, unit=1)
+#     #Função para escrita de números inteiros
+#     if func == '1':
+#         print('=' * 70)
+#         address = int(input('Escolha o endereço da tabela ModBus: ')) - 1
+#         valor = int(input('O valor INT que deve ser escrito: '))
+#         builder.add_16bit_int(valor)
+#         payload = builder.to_registers()
+#         payload = builder.build()
+#         client.write_registers(address, payload, skip_encode=True, unit=1)
 
-    #Função para escrita de boleano
-    elif func == '4':
-        print('=' * 70)
-        address = int(input('Escolha o endereço da tabela ModBus: ')) - 1
-        valor = int(input('Escolha 1 para True ou 0 para FALSE: '))
-        builder.add_bits([valor])
-        payload = builder.build()
-        client.write_registers(address, payload, skip_encode=True, unit=1)
+#     #Função para escrita de números float
+#     elif func == '2':
+#         print('=' * 70)
+#         address = int(input('Escolha o endereço da tabela ModBus: ')) - 1
+#         valor = float(input('O valor FLOAT que deve ser escrito: '))
+#         builder.add_32bit_float(valor)
+#         payload = builder.to_registers()
+#         payload = builder.build()
+#         client.write_registers(address, payload, skip_encode=True, unit=1)
 
-else:
-    print('Erro: Não foi possivel estabelecer conexão com o servidor')
+#     #Função para escrita de string
+#     elif func == '3':
+#         print('=' * 70)
+#         address = int(input('Escolha o endereço da tabela ModBus: ')) - 1
+#         valor = input('O que deve ser escrito: ')
+#         builder.add_string(valor)
+#         payload = builder.to_registers()
+#         payload = builder.build()
+#         client.write_registers(address, payload, skip_encode=True, unit=1)
+
+#     #Função para escrita de boleano
+#     elif func == '4':
+#         print('=' * 70)
+#         address = int(input('Escolha o endereço da tabela ModBus: ')) - 1
+#         valor = int(input('Escolha 1 para True ou 0 para FALSE: '))
+#         builder.add_bits([valor])
+#         payload = builder.build()
+#         client.write_registers(address, payload, skip_encode=True, unit=1)
+
+# else:
+#     print('Erro: Não foi possivel estabelecer conexão com o servidor')
