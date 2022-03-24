e724f666bc1ba6087de7abdef2c5f2d394eb00f0
Rafael Costa <93993679+rafaelbhcosta@users.noreply.github.com>
2022-03-24 10:22:32 -0300

    Update ler01.py

--- pyModbus/teste_de_esc_e_ler/ler01.py
+++ pyModbus/teste_de_esc_e_ler/ler01.py
@@ -28,16 +28,9 @@ if client.connect():
 #---------------------Funções-----------------------------
 #---------------------------------------------------------
 
-
-# int16, 32 e 64
-# float16, 32 e 64
-# Uint16, 32 e 64
-# bit
-# string 
-
     print('Escolha o tipo que você deseja ler: ')
     escolha = input('1- INT\n2- FLOAT\n3- BOLEANO\n4- STR \n5- DOOBLE\nEscolha:')
-    
+
     # Tipo de leitura INT
     if escolha == '1':
         linha()
@@ -53,7 +46,7 @@ if client.connect():
             print(decoder.decode_16bit_int())
 
         # Leitura de valores do tipo 32 bit
-        if tipo == '2':
+        elif tipo == '2':
             linha()
             address = int(input('Endereço da tabela: ')) - 1
             count   = 2
@@ -62,16 +55,16 @@ if client.connect():
             print(decoder.decode_32bit_int())
 
         # Leitura de valores do tipo 32 bit
-        if tipo == '3':
+        elif tipo == '3':
             linha()
             address = int(input('Endereço da tabela: ')) - 1
             count   = 4
             result = client.read_holding_registers(address, count, unit=1)
             decoder = BinaryPayloadDecoder.fromRegisters(result.registers)
             print(decoder.decode_64bit_int())
-    
+
     # Tipo de leitura FLOAT
-    if escolha == '2':
+    elif escolha == '2':
         linha()
         tipo = input('Tipo de bit para leitura:\n1- 16\n2- 32\n3- 64\n')
         
@@ -85,7 +78,7 @@ if client.connect():
             print(decoder.decode_16bit_float())
 
         # Leitura de valores do tipo 32 bit
-        if tipo == '2':
+        elif tipo == '2':
             linha()
             address = int(input('Endereço da tabela: ')) - 1
             count   = 2
@@ -94,7 +87,7 @@ if client.connect():
             print(decoder.decode_32bit_float())
 
         # Leitura de valores do tipo 64 bit
-        if tipo == '3':
+        elif tipo == '3':
             linha()
             address = int(input('Endereço da tabela: ')) - 1
             count   = 4
@@ -103,7 +96,7 @@ if client.connect():
             print(decoder.decode_64bit_float())
 
     # Tipo de leitura BOLEANO
-    if escolha == '3':
+    elif escolha == '3':
         linha()
         address = int(input('Endereço da tabela: ')) - 1
         count   = 1
@@ -111,46 +104,11 @@ if client.connect():
         decoder = BinaryPayloadDecoder.fromRegisters(result.registers)
         print(decoder.decode_bits()[0])
 
-    # if escolha == '3':
-    #     print('=' * 70)
-    #     address = int(input('Endereço da tabela: ')) - 1
-    #     count   = 3
-    #     result  = client.read_holding_registers(address, count,  unit=1)
-    #     decoder = BinaryPayloadDecoder.fromRegisters(result.registers)
-
-    #     # Função para leitura de strings
-    #     decoded = {
-    #         'string': decoder.decode_string(6),
-    #     }
-
-    #     print("-" * 70)
-    #     print("Decoded Data")
-    #     print("-" * 70)
-    #     for name, value in iteritems(decoded):
-    #         print("%s\t" % name, value)
-
-    # if escolha == '4':
-    #     print('=' * 70)
-    #     address = int(input('Endereço da tabela: ')) - 1
-    #     count   = 1
-    #     result  = client.read_holding_registers(address, count,  unit=1)
-    #     decoder = BinaryPayloadDecoder.fromRegisters(result.registers)
-
-    #     # Função para leitura de valores boleanos
-    #     decoded = {
-    #         'boleano': decoder.decode_bits()[0],
-    #     }
-
-    #     print("-" * 70)
-    #     print("Decoded Data")
-    #     print("-" * 70)
-    #     for name, value in iteritems(decoded):
-    #         print("%s\t" % name, value)
-
-    #---------------------------------------------------------
-
-    #Destinado para o dooble
-    # if escolha == '5':
-    #     print('=' * 70)
-    #     address = int(input('Endereço da tabela: ')) - 1
-    #     count   = 20
+    # Tipo de leitura STR
+    elif escolha == '4':
+        linha()
+        address = int(input('Endereço da tabela: ')) - 1
+        count   = 2
+        result = client.read_holding_registers(address, count, unit=1)
+        decoder = BinaryPayloadDecoder.fromRegisters(result.registers)
+        print(decoder.decode_string(4))
