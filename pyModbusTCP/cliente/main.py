110f1b277ac2891acf8a631896e6f32b5b8dd5d2
Rafael Costa <93993679+rafaelbhcosta@users.noreply.github.com>
2022-03-25 12:21:54 -0300

    att

--- pyModbusTCP/cliente/main.py
+++ pyModbusTCP/cliente/main.py
@@ -1,4 +1,11 @@
 from clientemodbus import ClientMODBUS
 
-c = ClientMODBUS('23.102.109.123',502)
+c = ClientMODBUS('127.0.0.1',502)
 c.atendimento()
+
+# O endereço a cima (127....) é o endereço de local host, ou seja só vai funcionar no seu computador
+# Caso esse endereço apresente erro de conexão pode ser tocado por 
+# 'localhost'
+# ou
+# '0.0.0.0'
+# Esse sistema de endereçamento vária de sitema para sitema (windows, linux, mac...) 
--- pyModbusTCP/servidor/main.py
+++ pyModbusTCP/servidor/main.py
@@ -2,3 +2,10 @@ from servidormodbus import ServidorMODBUS
 
 s = ServidorMODBUS(host_ip='127.0.0.1',port=502)
 s.run()
+
+# O endereço a cima (127....) é o endereço de local host, ou seja só vai funcionar no seu computador
+# Caso esse endereço apresente erro de conexão pode ser tocado por 
+# 'localhost'
+# ou
+# '0.0.0.0'
+# Esse sistema de endereçamento vária de sitema para sitema (windows, linux, mac...) 
